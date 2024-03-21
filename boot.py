d769c4cbae4f81ba156e30b6b0753a403984a4a1
MarianoR <marianodrodriguez03@gmail.com>
2024-03-21 01:33:43 -0300

    Se agrega .settings y gitignore.

--- boot.py
+++ boot.py
@@ -5,6 +5,6 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('conectando a la red...')
-    wlan.connect(SSID, Password)
+    wlan.connect("SSID", "Password")
     while not wlan.isconnected():
         pass
